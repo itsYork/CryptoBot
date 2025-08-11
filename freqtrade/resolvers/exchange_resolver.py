286e991b3533961be47b5bda7804e0db34d547e4
itsYork <120513919+itsYork@users.noreply.github.com>
2025-08-11 11:27:33 -0400

    Remove compiled artifacts

--- freqtrade/resolvers/exchange_resolver.py
+++
@@ -1,8 +0,0 @@
-class Exchange:
-    def __init__(self) -> None:
-        self._markets = {}
-
-class ExchangeResolver:
-    @staticmethod
-    def load_exchange(config, validate: bool = True):
-        return Exchange()
--- freqtrade/strategy/__init__.py
+++
@@ -1,7 +0,0 @@
-class IStrategy:
-    """Minimal strategy interface placeholder."""
-    timeframe: str = "1h"
-    can_short: bool = False
-
-    def __init__(self, *args, **kwargs):
-        pass
